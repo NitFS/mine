8db470b1cc5b31dc1214f2fb7ec64a4fe8a5185c
NitF1S <danil.gostev.06@mail.ru>
2023-10-06 19:48:54 +0600

    Mine1

---
+++ src/main/java/test/untitled4/Untitled4.java
@@ -0,0 +1,20 @@
+package test.untitled4;
+
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Untitled4 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+       Logger nm = getLogger();
+       nm.info("ZA WARDOOO");
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
